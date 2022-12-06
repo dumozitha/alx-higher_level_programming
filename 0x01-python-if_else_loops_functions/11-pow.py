243964c790ccc8a90e64498f749ad08ca5f90ab1
dumozitha <dumozitha@gmail.com>
2022-12-06 08:33:20 -0800

    updates

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# Author -Bamidele Adefolaju
+
+def pow(a, b):
+    return (a ** b)
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                                          0,1           All
+
